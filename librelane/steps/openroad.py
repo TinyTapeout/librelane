ea79617cb0e4dfd987a1bfe0b8bde220a2e440b2
Sylvain Munaut <tnt@246tNt.com>
2024-10-20 19:52:11 +0000

    steps/openroad: Add WriteAbstractLEF step
    Signed-off-by: Sylvain Munaut <tnt@246tNt.com>

--- librelane/steps/openroad.py
+++ librelane/steps/openroad.py
@@ -2162,6 +2162,7 @@ class CutRows(OpenROADStep):
         return os.path.join(get_script_dir(), "openroad", "cut_rows.tcl")
 
 
+@Step.factory.register()
 class WriteViews(OpenROADStep):
     """
     Write various layout views of an ODB design
@@ -2200,6 +2201,22 @@ class WriteCDL(OpenROADStep):
 
     def get_script_path(self):
         return os.path.join(get_script_dir(), "openroad", "write_cdl.tcl")
+
+
+@Step.factory.register()
+class WriteAbstractLEF(OpenROADStep):
+    """
+    Write Abstract LEF view of an ODB design
+    """
+
+    id = "OpenROAD.WriteAbstractLEF"
+    name = "Write Abstract LEF"
+    outputs = [
+        DesignFormat.LEF
+    ]
+
+    def get_script_path(self):
+        return os.path.join(get_script_dir(), "openroad", "write_abstract_lef.tcl")
 
 
 # Resizer Steps
